7a85bc9de2b4f2eb3c88ea6e7a7a720517c71930
season19840122 <season19840122@163.com>
2022-05-11 00:26:20 +0800

    initialize project

---
+++ DemoFromYoutube/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let a = "name";
+console.log(a);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLElBQUksQ0FBQyxHQUFXLE1BQU0sQ0FBQTtBQUN0QixPQUFPLENBQUMsR0FBRyxDQUFDLENBQUMsQ0FBQyxDQUFDIn0=
---
+++ DemoFromYoutube/src/index.ts
@@ -0,0 +1,18 @@
+let str = 'season';
+let str1: string;
+str1 = 'season2'
+
+let num: number = 1000
+let boo: boolean = true
+let n: null = null
+let un: undefined = undefined
+
+let test: any = 'name'
+
+// 数组
+let arr: string[] = ['a', 'b']
+let arr2: string[][] = [['aa', 'bb']]
+
+// 元组：指定类型的数组
+let tuple: [number, string, boolean] = [1, 'a', true]
+let tuple2: [string, string][] = [['a', 'b'], ['1', '2']]
