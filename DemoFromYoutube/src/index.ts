93770a83b06a478c85543b16bf0afb50005eb777
season19840122 <season19840122@163.com>
2022-05-11 01:28:42 +0800

    update to function

--- DemoFromYoutube/src/index.ts
+++ DemoFromYoutube/src/index.ts
@@ -15,4 +15,84 @@ let arr2: string[][] = [['aa', 'bb']]
 
 // 元组：指定类型的数组
 let tuple: [number, string, boolean] = [1, 'a', true]
-let tuple2: [string, string][] = [['a', 'b'], ['1', '2']]
+let tuple2 = ['a', 1 , true]
+let tuple3 = [1, 'a' , true]
+let tuple4: [string, string][] = [['a', 'b'], ['1', '2']]
+
+// 枚举
+enum Livestatus {
+    success = 0,
+    fail = -1,
+    streaming = 1
+}
+const staus = Livestatus.success
+console.log(staus);
+
+// Union
+let aaa: number | string;
+aaa = 1000
+aaa = 'str'
+
+// 自定义类型 type
+type A = number | string
+
+let a1: A
+a1 = 999
+a1 = "str"
+
+// interface
+type Card = {
+    name: string,
+    desc: string,
+}
+
+// interface 可以同名扩充，但是 type 不行
+// type Card = {
+//     age: number
+// }
+
+interface Card2 {
+    name: string,
+    desc: string,
+}
+
+interface Card2 {
+    age?: number
+}
+
+const obj: Card2 = {
+    name: 'bruce',
+    desc: '...'
+}
+
+// function
+function hello (a: string, b: string) {
+    return a + b;
+}
+
+function hello2 (a: string, b: string): number {
+    console.log(a + b);
+    return 999;
+}
+
+// 自动推导
+function hello3 (a: string, b: string) {
+    return 100;
+}
+
+function test2 (a: number) {
+    console.log(a);
+}
+
+function hello4 (name: string, age?: number) {
+    if( age === undefined) return -1;
+    test2(age);
+    return 100;
+}
+
+// 箭头函数也支持类型推导
+const func = () => {}
+
+const func2 = () => {
+    return 1
+}
